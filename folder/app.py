0882494fffd273a70a7603adcd2b8dddd8c79c31
Julia <shgvacop@gmail.com>
2026-08-31 15:25:02 +0300

    Create app.py

---
+++ folder/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/hello")
+def hello():
+    return "Привет из Python!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
